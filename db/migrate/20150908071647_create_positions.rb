b6b3a018b71bab6dcae49a13faa6ac90ada05925
Richie Bostock <richiebostock@Richies-MacBook-Air.local>
2015-09-16 22:30:37 +0800

    please work mr database

--- db/migrate/20150908071647_create_positions.rb
+++ db/migrate/20150908071647_create_positions.rb
@@ -1,6 +1,7 @@
 class CreatePositions < ActiveRecord::Migration
   def change
     create_table :positions do |t|
+      t.integer :index
       t.string :name
       t.belongs_to :user, index: true, foreign_key: true
       t.timestamps null: false
