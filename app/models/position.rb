7c38165d7129acb08d6d73e406323c42a042eb5b
Richie Bostock <richiebostock@Richies-MacBook-Air.local>
2015-09-25 16:11:07 +0900

    added max lengths for all inputs

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ActiveRecord::Base
   belongs_to :user
   has_many :technique_types, dependent: :destroy
-  validates :name, length: { maximum: 25 }
+  validates :name, length: { maximum: 30 }
 end
--- app/models/technique.rb
+++ app/models/technique.rb
@@ -2,5 +2,5 @@ class Technique < ActiveRecord::Base
   belongs_to :technique_type
   has_many :steps, dependent: :destroy
   validates :name, length: { maximum: 36 }  
-  validates :other_notes, length: { maximum: 500 }  
+  validates :other_notes, length: { maximum: 1000 }  
 end
