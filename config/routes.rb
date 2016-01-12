155cb91ecbacb586703ab8389b3daca6441368f1
Richie Bostock <richiebostock@Richies-MacBook-Air.local>
2016-01-12 20:21:20 +1000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root 'landing_page#index'
 
+  get "api" => proc { [404, {}, ['Invalid API endpoint']] }
+  get "api/*path" => proc { [404, {}, ['Invalid API endpoint']] }
+
+  get "/*path" => redirect("/?goto=%{path}")
+
+
   resources :positions do
     resources :technique_types do
       resources :techniques do
