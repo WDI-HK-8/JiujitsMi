2dec984a95b3ddbc5f20b8758735ba13b6fce9d8
Richie Bostock <richiebostock@Richies-MacBook-Air.local>
2016-01-12 14:47:26 +1000

    test

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Jiujitsmi
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins 'www.grappledash.com'
-        resource '*',
+        resource '/positions',
           :headers => :any,
           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
           :max_age => 0
